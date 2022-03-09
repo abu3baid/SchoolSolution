b145c5c18062ecf3234b91ff14520724b04449be
abu3beid <m76eef@gmail.com>
2022-03-09 04:13:56 +0200

    Add project files.

---
+++ SchoolProject/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolProject.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        [MaxLength(50)]
+        [MinLength(5)]
+        public string StudentName { get; set; }
+        public bool IsActive { get; set; }
+        [Range(5, 18)]
+        public int StudentAge { get; set; }
+
+    }
+}
