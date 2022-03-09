5074a4b9173177b300a54bb9044f5ba5d21ce4c8
abu3beid <m76eef@gmail.com>
2022-03-09 17:56:51 +0200

    using Factory Design Pattern and LINQ

---
+++ SchoolProject/Repository/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolProject.Models;
+
+namespace SchoolProject.Repository
+{
+    public interface IStudentRepository
+    {
+        public List<Student> GetAllStudents();
+        public void Create(Student student);
+        public void Delete(int Id);
+        public void Register(int studentId, int courseId);
+    }
+}
---
+++ SchoolProject/Repository/StudentRepository.cs
@@ -0,0 +1,39 @@
+﻿using SchoolProject.Context;
+using SchoolProject.Models;
+
+namespace SchoolProject.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly MyDbContext _myDbConnection;
+
+        public StudentRepository(MyDbContext myDbContext)
+        {
+            _myDbConnection = myDbContext;
+        }
+
+
+        public List<Student> GetAllStudents()
+        {
+            List<Student> students = (from stdsObj in _myDbConnection.Students 
+                                      select stdsObj).ToList();
+            return students;
+
+        }
+
+        public void Create(Student student)
+        {
+
+        }
+
+        public void Delete(int Id)
+        {
+
+        }
+
+        public void Register(int studentId, int courseId)
+        {
+
+        }
+    }
+}
