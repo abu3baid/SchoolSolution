099a15681eb91a5f2553cbaa55ad78868084d60c
abu3beid <m76eef@gmail.com>
2022-03-09 04:20:11 +0200

    Rooms Entity

--- SchoolProject/Models/Room.cs
+++ SchoolProject/Models/Room.cs
@@ -1,6 +1,18 @@
-﻿namespace SchoolProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolProject.Models
 {
     public class Room
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomId { get; set; }
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string RoomName { get; set; }
+        [Range(0, 25)]
+        public int Capicity { get; set; }
+
     }
 }
