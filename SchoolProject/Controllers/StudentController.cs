d791a5c57e862e2d48ab50dd4e1b1680dd24dd76
abu3beid <m76eef@gmail.com>
2022-03-09 04:55:56 +0200

    Student Controller

---
+++ SchoolProject/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Models;
+
+namespace SchoolProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        //List of Students
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Render The creation view
+        public ViewResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            return View();
+        }
+
+        public ViewResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(int studentId, int courseId)
+        {
+            return View();
+        }
+    }
+}
