c816fe16b83c7e04e17416a73b086753f1d04469
abu3beid <m76eef@gmail.com>
2022-03-09 04:41:25 +0200

    Added Context and startup

---
+++ SchoolProject/Context/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace SchoolProject.Context
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            base.OnConfiguring(dbContextOptionsBuilder);
+            var bulider = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot config = bulider.Build();
+            var conString = config.GetConnectionString("MyDbConnection");
+            dbContextOptionsBuilder.UseSqlServer(conString);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
